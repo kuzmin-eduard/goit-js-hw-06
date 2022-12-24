764d54a0db65e4f4bafd31845783e792e74901d8
kuzmin-eduard <115466306+kuzmin-eduard@users.noreply.github.com>
2022-12-24 18:37:49 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -14,3 +14,16 @@ const ingredients = [
   "Herbs",
   "Condiments",
 ];
+const ingredientsItem = document.querySelector("#ingredients");
+const ulIngredients = [];
+
+ingredients.forEach((ingredient) => {
+  const item = document.createElement("li");
+  item.className = "item";
+  item.textContent = ingredient;
+
+  ulIngredients.push(item);
+});
+
+ingredientsItem.append(...ulIngredients);
+console.log(ulIngredients);
