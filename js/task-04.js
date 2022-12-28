06ce8a7c20ed690fc2a18daa5892e5d1db955de5
kuzmin-eduard <115466306+kuzmin-eduard@users.noreply.github.com>
2022-12-28 16:09:23 +0200

    update

---
+++ js/task-04.js
@@ -0,0 +1,14 @@
+let value = document.querySelector("#value");
+let counterValue = 0;
+
+const buttonMinus = document.querySelector('button[data-action="decrement"]');
+const buttonPlus = document.querySelector('button[data-action="increment"]');
+
+buttonPlus.addEventListener("click", () => {
+  counterValue += 1;
+  value.textContent = counterValue;
+});
+buttonMinus.addEventListener("click", () => {
+  counterValue -= 1;
+  value.textContent = counterValue;
+});
---
+++ js/task-05.js
@@ -0,0 +1,14 @@
+const refs = {
+  input: document.querySelector("#name-input"),
+  span: document.querySelector("#name-output"),
+};
+
+refs.input.addEventListener("input", onInputChange);
+
+function onInputChange(event) {
+  if (event.target.value === "") {
+    refs.span.textContent = "Anonymous";
+  } else {
+    refs.span.textContent = event.target.value;
+  }
+}
